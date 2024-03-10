b809504285cf77b7ab887cc68928ef1ded54ea44
amankriet <amankriet@gmail.com>
2024-03-10 12:18:48 +0530

    initial todo app repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>ToDo App</h2>
+      <div className="container">
+        <div className="input-box">
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />{" "}
+          <button>Add Task</button>
+        </div>
+        <div className="list">
+          <ul>
+            <li>List</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }

f5ea7b07595eb6cf3b7cbf0f78bcc5df2b244183
amankriet <amankriet@gmail.com>
2024-04-30 21:31:33 +0530

    basic todo app features done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import "./App.css";
-import { useState } from "react";
+import "./App.css"
+import { useState } from "react"
+import deleteIcon from "./icons/delete.svg"
+import editIcon from "./icons/edit.svg"
+import addButton from "./icons/add.svg"
+import updateButton from "./icons/update-arrow.svg"
 
 function App() {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState("")
+  const [list, setList] = useState([])
+  const [taskIndex, setTaskIndex] = useState(-1)
+
+  const handleInput = (e) => {
+    setInput(e.target.value)
+  }
+
+  const handleTask = () => {
+    setList([...list, input])
+    setInput("");
+  }
+  const handleUpdate = () => {
+    if (taskIndex !== -1) {
+      list.splice(taskIndex, 1, input)
+      setInput("")
+      setTaskIndex(-1)
+    } else {
+      alert("No task selected for update")
+    }
+  }
+
+  const handleDelete = (i) => {
+    const filterList = list.filter((ele) => ele !== list[i])
+    setList(filterList)
+  }
+
+  const handleEdit = (i) => {
+    const filterList = list.filter((ele) => ele === list[i])
+    setInput(filterList[0])
+    setTaskIndex(i);
+  }
 
   return (
     <div className="App">
       <h2>ToDo App</h2>
       <div className="container">
-        <div className="input-box">
+        <form className="input-box">
           <input
             type="text"
             value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />{" "}
-          <button>Add Task</button>
-        </div>
+            className="todo-input"
+            placeholder="Enter new task"
+            onChange={(e) => handleInput(e)}
+          />
+          {taskIndex > -1 ?
+            <img src={updateButton} alt="Update Button" className="update-button" title="Update Task" onClick={handleUpdate} />
+            :
+            <img src={addButton} alt="Add button" className="add-button" title="Add Task" onClick={handleTask} />}
+        </form>
         <div className="list">
           <ul>
-            <li>List</li>
+            {list.map((item, i) => <li key={i}>{item} <img src={deleteIcon} alt="Delete" className="icon-button del-icon" title="Delete" onClick={() => handleDelete(i)} />
+              <img src={editIcon} alt="Edit" title="Edit" className="icon-button edit-icon" onClick={() => handleEdit(i)} /> </li>)}
           </ul>
         </div>
       </div>
